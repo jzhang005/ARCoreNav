f771eb03b2bdb5acc9be4656e441ddfdd882bb7f
jzhang005 <jzhang005@citymail.cuny.edu>
2019-05-15 10:57:47 -0400

    ARCoreNav

---
+++ Assets/Scripts/CamZoom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamZoom : MonoBehaviour
+{
+    Vector3 touchStart;
+    public GameObject player;
+    public float perspectiveZoomSpeed = .5f;
+    public float orthoZoomSpeed = .5f;
+
+    Vector3 pos;
+    Vector3 cameraPos;
+
+    void Update()
+    {
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            this.transform.position = new Vector3(touchZero.position.x / 51.8f, 20f, touchZero.position.y / 49.33f);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float deltaMagnitudediff = prevTouchDeltaMag - touchDeltaMag;
+
+            Camera.main.fieldOfView += deltaMagnitudediff * perspectiveZoomSpeed;
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 50f, 115f);
+        }
+        else
+        {
+            pos = player.transform.position;
+            cameraPos.x = pos.x;
+            cameraPos.y = 20f;
+            cameraPos.z = pos.z;
+            this.transform.position = cameraPos;
+        }
+
+    }
+}
---
+++ Assets/Scripts/JoystickControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickControl : MonoBehaviour
+{
+    protected Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 5f,
+                                         0f,
+                                         joystick.Vertical * 5f);
+    }
+}
